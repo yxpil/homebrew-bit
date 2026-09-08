973111d0adb5238ef7e1f53f10d3f22829eb24e3
枕忶YxPil <waveluyubook2020@outlook.com>
2026-09-08 17:32:05 +0800

    bit 0.5.35

--- Casks/bit.rb
+++ Casks/bit.rb
@@ -1,9 +1,9 @@
 cask "bit" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.5.34"
-  sha256 arm:   "aeb0758765184d2b2ca7b89f88b09afb3e309a8cd2e0e589da7728b67571c7a9",
-         intel: "7f39ecf9b43f80a07444bd2c95e62f6aa7b3ef5027ac21f8ecd76d222f7f8f37"
+  version "0.5.35"
+  sha256 arm:   "198bb845eb79b74b25c405b8ef33321504eb86d23313a53aabd6f569857a020b",
+         intel: "285750208e3c6129c65c0c0fd300f332908dc989a3aaead6813a9ff523729394"
 
   url "https://github.com/yxpil/bit/releases/download/v#{version}/BIT_#{version}_#{arch}.dmg"
   name "BIT"
